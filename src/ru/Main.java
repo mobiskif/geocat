710d2935fba274448dc7fbc5d1cc72133741868a
user <mobiskif@hotmail.com>
2017-11-01 16:43:42 +0300

    Initial commit

---
+++ src/ru/Main.java
@@ -0,0 +1,16 @@
+package ru;
+
+
+public class Main {
+    String reg[] = {"BillNumber","ServiceGroup","DataPay","pass","email"};
+    String list[] = {"ServiceGroup","IP","DataPay","lng","lat"};
+    String s_commands[] = {"help","reg","list","join"};
+    String c_commands[] = {"help","servlist","serv"};
+
+
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("test");
+    }
+}
