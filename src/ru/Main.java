50bf004ae5e9da82b762d4261601923fedf7a582
user <mobiskif@hotmail.com>
2017-11-01 17:23:50 +0300

    01.11.17

--- src/ru/Main.java
+++ src/ru/Main.java
@@ -1,16 +1,20 @@
 package ru;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
-    String reg[] = {"BillNumber","ServiceGroup","DataPay","pass","email"};
-    String list[] = {"ServiceGroup","IP","DataPay","lng","lat"};
-    String s_commands[] = {"help","reg","list","join"};
-    String c_commands[] = {"help","servlist","serv"};
-
-
+    String group[] = {"Товары бытовые", "Услуги бытовые", "Услуги медицинские", "Усдуги информационные"};
+    String reg[] = {"group", "email", "bill", "dpay","pass"};
+    String list[] = {"group","ip","lng","lat"};
+    String s_commands[] = {"group", "reg","list","join"};
+    String c_commands[] = {"list","serv"};
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("test");
-    }
+        Map<String, String[]> synt = new HashMap();
+        synt.put("reg", new String[] {"qwre","qwerwe","qwerwer"});
+        synt.put("list", new String[] {"qwre","qwerwe","qwerwer"});
+        System.out.println(synt);
+   }
 }
