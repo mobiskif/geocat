2c86d94289c3c7752ad3920ae9ee453cb4cdf97e
user <mobiskif@hotmail.com>
2017-11-02 15:53:18 +0300

    01.11.17

--- src/ru/Main.java
+++ src/ru/Main.java
@@ -2,18 +2,21 @@ package ru;
 
 
 import java.lang.reflect.Array;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
-    String group[] = {"Товары бытовые", "Услуги бытовые", "Услуги медицинские", "Усдуги информационные"};
-    String reg[] = {"group", "email", "bill", "dpay", "pass"};
+    String group[] = {"sb2b", "sb2c", "tb2b", "tb2c"};
+    String reg[] = {"group", "email", "bill", "pass", "dpay"};
     String list[] = {"group", "ip", "lng", "lat"};
     String s_commands[] = {"group", "reg", "list", "join"};
     String c_commands[] = {"list", "serv"};
 
+
     public static void main(String[] args) {
+        Map<String, String> hashmap0 = new HashMap<String, String>() {{
+            put("a", "b");
+            put("aa", "bb");
+        }};
 
         Map<String, String[]> hashmap1 = new HashMap<String, String[]>();
         hashmap1.put("k1.1", new String[]{"1", "2", "3"});
@@ -33,7 +36,7 @@ public class Main {
 */
 
         Map<String, Map> hashmap2 = new HashMap();
-        hashmap2.put("k2.1", hashmap1);
+        hashmap2.put("k2.1", hashmap0);
         hashmap2.put("k2.2", hashmap1);
         hashmap2.put("k2.3", hashmap1);
 
@@ -41,29 +44,48 @@ public class Main {
         hashmap3.put("k3.1", hashmap2);
         hashmap3.put("k3.2", hashmap2);
 
-        System.out.println(printmap(hashmap3));
+        System.out.println(hashmap1);
 
+        System.out.println(fill(new ArrayList()));
+        System.out.println(fill(new HashSet()));
+        System.out.println(fill(new HashMap()));
 
     }
 
+    static Collection fill(Collection c) {
+        c.add("dog");
+        c.add("dog");
+        c.add("cat");
+        return c;
+    }
+
+    static Map fill(Map m) {
+        m.put("dog", "Bosco");
+        m.put("dog", "Spot");
+        m.put("cat", "Rags");
+        return m;
+    }
 
     static String printarr(String[] arr) {
-        String s= "{";
+        String s = "{";
         for (String elem : arr) {
             //System.out.println(elem);
-            if (s=="{") s+=elem;
-            else s+=", "+elem;
+            if (s == "{") s += elem;
+            else s += ", " + elem;
         }
-        return s+"}\n";
+        return s + "}\n";
     }
 
     static String printmap(Map<String, Map> arr) {
-        String ss= "\n";
+        String ss = "\n";
         for (Map.Entry entry : arr.entrySet()) {
-            if (!entry.getValue().getClass().isArray()) ss = ss + entry.getKey() + " = " + printmap((Map) entry.getValue());
-            else ss = ss + "\t"+entry.getKey() + " = " + printarr((String[])entry.getValue());
+            if (entry.getValue().getClass().isMemberClass())
+                ss = ss + entry.getKey() + " = " + printmap((Map) entry.getValue());
+            else if (entry.getValue().getClass().isArray())
+                ss = ss + "\t" + entry.getKey() + " = " + printarr((String[]) entry.getValue());
+            else ss = ss + entry.getValue() + "\n";
         }
-        return ss+"";
+        return ss + "";
     }
 
 }
